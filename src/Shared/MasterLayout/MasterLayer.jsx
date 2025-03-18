92c4a1c9424d9f79ee75d4aa552b12bc203ce8ce
HalimMahmoud <halim_mahmoud50@yahoo.com>
2025-03-18 02:55:10 +0200

    edit sidebar

--- src/Shared/MasterLayout/MasterLayer.jsx
+++ src/Shared/MasterLayout/MasterLayer.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 
-import SideBar from "../SideBar/SideBar";
+import SideBar from "../Sidebar/Sidebar";
 import { useLoginData } from "../../Hooks/useLoginData";
 
 // eslint-disable-next-line react/prop-types

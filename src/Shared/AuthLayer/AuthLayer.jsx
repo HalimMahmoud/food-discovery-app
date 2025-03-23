748a8ef2d626610d863f95acf797950a0e1b190f
HalimMahmoud <halimmahmoud50@yahoo.com>
2025-03-23 03:38:50 +0200

    fix bg

--- src/Shared/AuthLayer/AuthLayer.jsx
+++ src/Shared/AuthLayer/AuthLayer.jsx
@@ -1,5 +1,6 @@
 import { Outlet, Navigate } from "react-router-dom";
 import Logo from "../../assets/4 3.png";
+import BgImg from "../../assets/ella-olsson-C1Q3qOTlegg-unsplash 1.png";
 
 import { useContext } from "react";
 import { AuthContext } from "../../Contexts/AuthContext";
@@ -11,7 +12,7 @@ export default function AuthLayer() {
   }
 
   return (
-    <div className="layout vh-100 ">
+    <div className="layout vh-100" style={{ backgroundImage: `url(${BgImg})` }}>
       <div className="overlay d-flex justify-content-center align-items-center">
         <div className="container-fluid w-50 bg-white rounded col-md-5 px-5 py-3">
           <div className="d-flex flex-column align-items-center h-50 justify-content-between">
